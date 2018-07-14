1400edd9145e98c8fd55877c82a105eb72208f09
Joe <joetho@zillowgroup.com>
2018-07-14 13:53:30 -0700

    Update modules

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
  /* jshint esversion: 6 */
 var BLT = require("bitcoin-live-transactions")
-var bitcoin = new BLT()
+var bitcoinjs = require('bitcoinjs-lib')
 var bitmex = require('./bitmex')
+
 
 // Putting a closure so the base seed is not publicly accessible.
 module.exports.HWallet = (() => {
   let BASE_SEED = null;
+  let counterSeed = null;
   const seedCounter = 0;
 
   class HWallet {
     constructor (seed) {
       BASE_SEED = seed;
-      bitcoin.connect();
-    },
-    newReceive () {
-      let address = this.genAddress();
-      bitcoin.events.on(this.address,tx => {
+      counterSeed = seed;
 
-        // Remove this listener for memory management
-        bitcoin.events.removeListener(this.address, arguments.callee);
+      bitcoin.connect();
+    }
+    newReceive (amount) {
+      let { address } = this.genAddress();
+
+      bitcoin.events.on(this.address, tx => {
+        tx.vout.forEach(out => {
+          if (out.address === this.address && out.amount === amount) {
+            console.info('Wooohoo, payment received');
+
+            // Remove this listener for memory management
+            bitcoin.events.removeListener(this.address, arguments.callee);
+          }
+        })
       });
 
     }
     setAmount () {
 
     }
-    address () {
-      return this.address
+    genAddress () {
+      let pk = sha256()
     }
     hedgeHash () {
 

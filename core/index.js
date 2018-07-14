fc1c6293a4d4daa1c3023e7f8c8211116f0c3200
Joe <joetho@zillowgroup.com>
2018-07-14 14:34:12 -0700

    Fix send money

--- core/index.js
+++ core/index.js
@@ -7,8 +7,12 @@ var bitmex = require('./bitmex')
 // Putting a closure so the base seed is not publicly accessible.
 module.exports.HWallet = (() => {
   let BASE_SEED = null;
-  let counterSeed = null;
-  const seedCounter = 0;
+  let hashkey = null;
+  let hashCounter = 0;
+  let usedMap = {};
+  const sha256 = bitcoinjs.crypto.sha256;
+
+
 
   class HWallet {
     constructor (seed) {
@@ -24,22 +28,60 @@ module.exports.HWallet = (() => {
         tx.vout.forEach(out => {
           if (out.address === this.address && out.amount === amount) {
             console.info('Wooohoo, payment received');
-
+            // TODO: Add bitmex Hedge Payment function
             // Remove this listener for memory management
             bitcoin.events.removeListener(this.address, arguments.callee);
           }
         })
       });
-
     }
     setAmount () {
 
     }
     genAddress () {
-      let pk = sha256()
+      // Deteministically generate a new private key and increment.
+      hashkey = sha256(hashkey);
+      hashCounter++;
+      usedMap[hashCounter] = true;
+
+      // Create the key pair and return it.
+      const pair = bitcoinjs.ECPair.fromPrivateKey(hashkey);
+
+      return bitcoinjs.payments.p2pkh({
+        pubkey: pair.publicKey
+      });
     }
     hedgeHash () {
 
+    }
+    /**
+     * send - sends money from the local wallet to a specified address
+     * @param {string} toAddress - the address we're sending money to
+     * @param {number} amount - number of satoshis we're sending.
+     * @returns {Promise}
+     */
+    send (toAddress, amount) {
+      const txb = new bitcoin.TransactionBuilder(regtest)
+
+      txb.addInput(unspent0.txId, unspent0.vout);
+      txb.addOutput(toAddress, amount);
+      txb.addOutput(myAddress, remainder);
+      txb.sign(0, pk1);
+
+      tx = txb.build();
+
+      return new Promise((resolve, reject) => {
+        regtestUtils.broadcast(tx.toHex(), err => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          // TODO: Clean up the spent outputs,
+          // TODO: Update Bitmex!
+          console.log(`Woohoo sent tx to ${toAddress}`);
+          resolve(tx.id());
+        });
+      });
     }
   }
 

49adea93897747ca7f732756107fedb48f0d28c3
Joe <joetho@zillowgroup.com>
2018-07-15 11:11:35 -0700

    Update index

--- core/index.js
+++ core/index.js
@@ -23,24 +23,28 @@ module.exports.HWallet = (() => {
     }
 
     newReceive (amount) {
-      let { address } = this._genAddress();
+      let aObj = this._genAddress();
       return {
         address,
         wait: new Promise((resolve, reject) => {
-          bitcoin.events.on(this.address, tx => {
-            tx.vout.forEach(async out => {
-              if (out.address === this.address && out.amount === amount) {
+          bitcoin.events.on(aObj.address, tx => {
+            tx.vout.forEach(async (out,i) => {
+              if (out.address === aObj.address && out.amount === amount) {
                 //await deposit = bitmex.fetchDepositAddress();
                 //this.depositOrSplit(amount);
                 // Remove this listener for memory management
+                amountLocal += amount;
+                aObj.txid = txid;
+                aObj.amount = amount;
+                aObj.vout = i;
                 bitcoin.events.removeListener(this.address, arguments.callee);
+                resolve(txid);
               }
             });
           });
         })
       };
     }
-    despo
     _genAddress () {
       hashkey = sha256(hashkey);
       hashCounter++;
@@ -48,12 +52,20 @@ module.exports.HWallet = (() => {
       // Create the key pair and return it.
       const pair = bitcoinjs.ECPair.fromPrivateKey(hashkey);
 
-      // Deteministically generate a new private key and increment.
-      usedMap[pair] = hashkey;
-
-      return bitcoinjs.payments.p2pkh({
+      // The bitcoin
+      const { address } = bitcoinjs.payments.p2pkh({
         pubkey: pair.publicKey
       });
+
+      // Deteministically generate a new private key and increment.
+      usedMap[hashCounter] = {
+        sk: pair.pri,
+        pk: pair.publicKey,
+        address,
+        amount: 0
+      };
+
+      return usedMap[hashCounter];
     }
     /**
      * send - sends money from the local wallet to a specified address
@@ -62,19 +74,54 @@ module.exports.HWallet = (() => {
      * @returns {Promise}
      */
     send (toAddress, amount, bitmex) {
-      const txb = new bitcoin.TransactionBuilder(regtest)
-      var a = Object.keys(usedMap)
 
-      txb.addInput(unspent0.txId, unspent0.vout);
-      txb.addOutput(toAddress, amount);
-      txb.addOutput(myAddress, remainder);
-      txb.sign(0, pk1);
-
-      tx = txb.build();
-
-      amountLocal -= amount;
+      const txb = new bitcoin.TransactionBuilder(regtest);
+      // Sort out the amount of money we send back, by the smallest first.
+      let txids = Object.keys(usedMap)
+        .filter(i => usedMap[i].amount)
+        .sort((a,b) => {
+          return usedMap[a].amount - usedMap[b].amount;
+        })
 
       return new Promise((resolve, reject) => {
+        if (amountLocal < amount) {
+          reject()
+        }
+
+        let acc = 0;
+        let i = 0;
+        let remainder = 0;
+        while (acc < amount) {
+          acc += usedMap[i++].amount;
+        }
+        if (acc > amount) {
+          remainder = acc - amount;
+        }
+
+        // Add inputs
+        for (var n = 0; n < i; n++) {
+          txb.addInput(usedMap[i].txid, usedMap[i].vout);
+        }
+
+        txb.addOutput(toAddress, amount);
+        txb.addOutput(usedMap[i - 1], remainder);
+
+        // Add sigs
+        for (var n = 0; n < i; n++) {
+          txb.sign(0, usedMap[i].sk);
+
+          if (n != i - 1) {
+            delete usedMap[i];
+          }
+        }
+
+        tx = txb.build();
+
+        usedMap[i - 1].amount = remainder;
+        usedMap[i - 1].txid = tx.id();
+        usedMap[i - 1].vout = 1;
+
+
         regtestUtils.broadcast(tx.toHex(), err => {
           if (err) {
             reject(err);
@@ -85,6 +132,7 @@ module.exports.HWallet = (() => {
           // TODO: Update Bitmex!
           console.log(`Woohoo sent tx to ${toAddress}`);
           resolve(tx.id());
+          amountLocal -= amount;
         });
       });
     }

742ce461786b557f36d869cad05ac21f612f32cc
tofurocks <mattwall0@gmail.com>
2018-07-14 21:18:45 -0700

    Add files via upload
    bitmex functionality

---
+++ core/matt/hlib.js
@@ -0,0 +1,106 @@
+const fetch = require('node-fetch');
+const crypto = require('crypto');
+const qs = require('qs');
+ 
+const apiKey = "bmtmfpw-vromkcpFsHxaEUdg";
+const apiSecret = "ouN9KgJ5eIZiXzWtw6-KEy_CGtAbSwKWn_nCRdYqqpVW2ajC";
+
+function makeRequest(verb, endpoint, data = {}) {
+  const apiRoot = '/api/v1/';
+ 
+  const expires = new Date().getTime() + (60 * 1000);  // 1 min in the future
+ 
+  let query = '', postBody = '';
+  if (verb === 'GET')
+    query = '?' + qs.stringify(data);
+  else
+    // Pre-compute the reqBody so we can be sure that we're using *exactly* the same body in the request
+    // and in the signature. If you don't do this, you might get differently-sorted keys and blow the signature.
+    postBody = JSON.stringify(data);
+ 
+  const signature = crypto.createHmac('sha256', apiSecret)
+    .update(verb + apiRoot + endpoint + query + expires + postBody).digest('hex');
+ 
+  const headers = {
+    'content-type': 'application/json',
+    'accept': 'application/json',
+    // This example uses the 'expires' scheme. You can also use the 'nonce' scheme. See
+    // https://www.bitmex.com/app/apiKeysUsage for more details.
+    'api-expires': expires,
+    'api-key': apiKey,
+    'api-signature': signature,
+  };
+ 
+  const requestOptions = {
+    method: verb,
+    headers,
+  };
+  if (verb !== 'GET') requestOptions.body = postBody;  // GET/HEAD requests can't have body
+ 
+  const url = 'https://testnet.bitmex.com' + apiRoot + endpoint + query;
+ 
+  return fetch(url, requestOptions).then(response => response.json()).then(
+    response => {
+        //TODO: handle error?
+      //if ('error' in response) throw new Error(response.error.message);
+      return response;
+    },
+    error => console.error('Network error', error),
+  );
+}
+ 
+async function fetchMarkPrice() {
+  try {
+    const result = await makeRequest('GET',  'instrument', {
+      symbol: 'XBT',
+      columns: 'markPrice'
+    });
+    return result[0].markPrice
+  } catch (e) {
+    console.error(e);
+  };
+};
+
+async function fetchDepositAddress() {
+    try {
+      const result = await makeRequest('GET',  'user/depositAddress', {
+        currency: 'XBt'
+      });
+      return result;
+    } catch (e) {
+      console.error(e);
+    };
+  };
+
+  async function submitOrder(orderSize) {
+    try {
+      const result = await makeRequest('POST',  'order', {
+        symbol: 'XBTUSD',
+        orderQty: orderSize,
+        ordType: 'Market'
+      });
+      return result;
+    } catch (e) {
+      console.error(e);
+    };
+  };
+
+  async function getPositionSize() {
+    try {
+      const result = await makeRequest('GET',  'position', {
+        symbol: 'XBTUSD',
+      });
+      return result[0].currentQty;
+    } catch (e) {
+      console.error(e);
+    };
+  };
+
+
+  
+module.exports = {
+    fetchMarkPrice : fetchMarkPrice,
+    fetchDepositAddress : fetchDepositAddress,
+    submitOrder : submitOrder,
+    getPositionSize : getPositionSize
+}
---
+++ core/matt/index.js
@@ -0,0 +1,52 @@
+const hlib = require('./hlib');
+
+var request = require('request');
+var crypto = require('crypto');
+
+var apiKey = "bmtmfpw-vromkcpFsHxaEUdg";
+var apiSecret = "ouN9KgJ5eIZiXzWtw6-KEy_CGtAbSwKWn_nCRdYqqpVW2ajC";
+
+//fake balance for testing purposes
+var fakeBalance = 0.5
+
+var posSize = null;
+var apiFetches = [];
+
+/* var price =  hlib.fetchMarkPrice().then(res => {
+    console.log("Current price is: ");
+    console.log(res)
+});
+
+var add =  hlib.fetchDepositAddress().then(res => { 
+    console.log("Deposit address is: ");
+    console.log(res);
+});
+
+var pos = hlib.getPositionSize().then(res => {
+    console.log("Position size is: ");
+    console.log(res);
+}) */
+
+var pos = hlib.getPositionSize();
+var price = hlib.fetchMarkPrice();
+
+//var ord = hlib.submitOrder(10);
+
+Promise.all([pos, price]).then( res => {
+    console.log("Position size is: ")
+    console.log(res[0])
+    console.log("Current price is: ")
+    console.log(res[1])
+    console.log("Contracts needed to hedge: ")
+    var targPos = fakeBalance * res[1];
+    console.log(targPos)
+    console.log("Contracts to add/remove to be balanced: ")
+    var change = res[0] - targPos;
+    console.log(change)
+});
+
+
+
+
+
+
